de5d1298963f33c9f07418c15e54743f6d128406
KANGSUNGHO <kangsho15@gc.gachon.ac.kr>
2020-08-23 00:53:52 +0900

    component scan & DI

--- noticeboard/src/main/java/polymorphism/LgTV.java
+++ noticeboard/src/main/java/polymorphism/LgTV.java
@@ -1,32 +1,34 @@
 package polymorphism;
 
-public class LgTV implements TV {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component("tv")
+public class LgTV implements TV {
+	@Autowired
+	private Speaker speaker;
+	
 	public LgTV() {
 		System.out.println("===> LgTV 객체 생성");
 	}
 	@Override
 	public void powerOn() {
 		System.out.println("LgTV -- 전원 키기");
-
 	}
 
 	@Override
 	public void powerOff() {
 		System.out.println("LgTV -- 전원 끄기");
-
 	}
 
 	@Override
 	public void volumeUp() {
-		System.out.println("LgTV -- 볼륨 키우기");
-
+		speaker.volumeUp();
 	}
 
 	@Override
 	public void volumeDown() {
-		System.out.println("LgTV -- 볼륨 내리기");
-
+		speaker.volumeDown();
 	}
 
 }
