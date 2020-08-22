470febfad89d9032424a5936f91098cbfebe4188
KANGSUNGHO <kangsho15@gc.gachon.ac.kr>
2020-08-22 23:43:34 +0900

    DI - 생성자 인젝션

---
+++ noticeboard/src/main/java/polymorphism/AppleSpeaker.java
@@ -0,0 +1,20 @@
+package polymorphism;
+
+public class AppleSpeaker implements Speaker {
+
+	public AppleSpeaker() {
+		System.out.println("===> AppleSpeaker 객체 생성");
+	}
+	@Override
+	public void volumeUp() {
+		System.out.println("AppleSpeaker 소리 키우기");
+
+	}
+
+	@Override
+	public void volumeDown() {
+		System.out.println("AppleSpeaker 소리 줄이기");
+
+	}
+
+}
--- noticeboard/src/main/java/polymorphism/SamsungTV.java
+++ noticeboard/src/main/java/polymorphism/SamsungTV.java
@@ -1,14 +1,26 @@
 package polymorphism;
 
 public class SamsungTV implements TV {
+	private Speaker speaker;
+	private int price;
 	
 	public SamsungTV() {
-		System.out.println("==> SamsungTV 객체 생성");
+		System.out.println("==> SamsungTV(1) 객체 생성");
+	}
+	
+	public SamsungTV(Speaker speaker) {
+		System.out.println("==> SamsungTV(2) 객체 생성");
+		this.speaker = speaker;
+	}
+	public SamsungTV(Speaker speaker, int price) {
+		System.out.println("==> SamsungTV(3) 객체 생성");
+		this.speaker = speaker;
+		this.price = price;
 	}
 
 	@Override
 	public void powerOn() {
-		System.out.println("SamsungTV -- 전원 키기");
+		System.out.println("SamsungTV -- 전원 키기, 가격: " + price );
 
 	}
 
