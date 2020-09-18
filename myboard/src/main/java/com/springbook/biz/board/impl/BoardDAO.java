5cf0576c6f79cf3da4f9c4c96fb7b7f921b2c4ae
KANGSUNGHO <kangsho15@gc.gachon.ac.kr>
2020-09-19 02:08:25 +0900

    mybatis 설정 파일 작성

--- myboard/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
+++ myboard/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
@@ -1,147 +1,40 @@
 package com.springbook.biz.board.impl;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Repository;
+import org.apache.ibatis.session.SqlSession;
 
 import com.springbook.biz.board.BoardVO;
-import com.springbook.biz.common.JDBCUtil;
+import com.springbook.biz.util.SqlSessionFactoryBean;
 
 // DAO(Data Access Object)
-@Repository("boardDAO")
 public class BoardDAO {
-	// JDBC 관련 변수
-	private Connection conn = null;
-	private PreparedStatement stmt = null;
-	private ResultSet rs = null;
+	private SqlSession mybatis;
 	
-	// SQL 명령어들
-	private final String BOARD_INSERT = "insert into board(seq,title,writer,content,regdate) values((select nvl(max(seq),0) + 1 from board b),?,?,?,now())";
-	private final String BOARD_UPDATE = "update board set title=?, content=? where seq=?";
-	private final String BOARD_DELETE = "delete from board where seq=?";
-	private final String BOARD_GET = "select * from board where seq = ?";
-	private final String BOARD_LIST = "select * from board order by seq desc";
+	public BoardDAO() {
+		mybatis = SqlSessionFactoryBean.getSqlSessionInstance();
+	}
 	
-	// 조건 검색 SQL 명령어
-	private final String BOARD_LIST_T = 
-				"select * from board where title like CONCAT('%',?,'%') order by seq desc";
-	private final String BOARD_LIST_C = 
-				"select * from board where content like CONCAT('%',?,'%') order by seq desc"; 
-	
-	// CRUD 기능의 메소드 구현
-	// 글 등록
 	public void insertBoard(BoardVO vo) {
-		System.out.println("==> JDBC로 insertBoard() 기능 처리");
-		try {
-			conn = JDBCUtil.getConnection();
-			System.out.println("conn: " + conn);
-			stmt = conn.prepareStatement(BOARD_INSERT);
-			stmt.setString(1,vo.getTitle());
-			stmt.setString(2,vo.getWriter());
-			stmt.setString(3,vo.getContent());
-			stmt.executeUpdate();
-		} catch(Exception e) {
-			e.printStackTrace();
-		} finally {
-			JDBCUtil.close(stmt, conn);
-		}
+		mybatis.insert("Board.insertBoard",vo);
+		mybatis.commit();
 	}
 	
-	// 글 수정
 	public void updateBoard(BoardVO vo) {
-		System.out.println("===> JDBC로 updateBoard() 기능처리");
-		try {
-			conn = JDBCUtil.getConnection();
-			stmt = conn.prepareStatement(BOARD_UPDATE);
-			stmt.setString(1,vo.getTitle());
-			stmt.setString(2, vo.getContent());
-			stmt.setInt(3, vo.getSeq());
-			stmt.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			JDBCUtil.close(stmt, conn);
-		}
+		mybatis.update("Board.updateBoard",vo);
+		mybatis.commit();
 	}
 	
-	// 글 삭제
 	public void deleteBoard(BoardVO vo) {
-		System.out.println("===> JDBC로 deleteBoard() 기능 처리");
-		try {
-			conn = JDBCUtil.getConnection();
-			stmt = conn.prepareStatement(BOARD_DELETE);
-			stmt.setInt(1, vo.getSeq());
-			stmt.executeUpdate();
-		} catch(Exception e) {
-			e.printStackTrace();
-		} finally {
-			JDBCUtil.close(stmt,conn);
-		}
+		mybatis.delete("Board.deleteBoard", vo);
+		mybatis.commit();
 	}
 	
-	// 글 상세 조회
 	public BoardVO getBoard(BoardVO vo) {
-		System.out.println("===> JDBC로 getBoard() 기능 처리");
-		BoardVO board = null;
-		try {
-			conn = JDBCUtil.getConnection();
-			stmt = conn.prepareStatement(BOARD_GET);
-			stmt.setInt(1, vo.getSeq());
-			rs = stmt.executeQuery();
-			if(rs.next()) {
-				board = new BoardVO();
-				board.setSeq(rs.getInt("SEQ"));
-				board.setTitle(rs.getString("TITLE"));
-				board.setWriter(rs.getString("WRITER"));
-				board.setContent(rs.getString("CONTENT"));
-				board.setRegDate(rs.getDate("REGDATE"));
-				board.setCnt(rs.getInt("CNT"));
-			}
-		} catch(Exception e) {
-			e.printStackTrace();
-		} finally {
-			JDBCUtil.close(rs, stmt,conn);
-		}
-		return board;
+		return (BoardVO) mybatis.selectOne("Board.getBoard", vo);
 	}
 	
-	// 글 목록 조회
 	public List<BoardVO> getBoardList(BoardVO vo){
-		System.out.println("===> JDBC로 getBoardList() 기능 처리");
-		List<BoardVO> boardList = new ArrayList<BoardVO>();
-		try {
-			conn = JDBCUtil.getConnection();
-			System.out.println("SearchCondition : " + vo.getSearchCondition() );
-			if(vo.getSearchCondition().equals("TITLE")) {
-				System.out.println("TITLE 접속");
-				stmt = conn.prepareStatement(BOARD_LIST_T);
-			} else if(vo.getSearchCondition().equals("CONTENT")) {
-				System.out.println("CONTENT 접속");
-				stmt = conn.prepareStatement(BOARD_LIST_C);
-			}
-			stmt.setString(1, vo.getSearchKeyword());
-			System.out.println("SearchKeyword : " + vo.getSearchKeyword());
-			
-			rs = stmt.executeQuery();
-			while(rs.next()) {
-				BoardVO board = new BoardVO();
-				board.setSeq(rs.getInt("SEQ"));
-				board.setTitle(rs.getString("TITLE"));
-				board.setWriter(rs.getString("WRITER"));
-				board.setContent(rs.getString("CONTENT"));
-				board.setRegDate(rs.getDate("REGDATE"));
-				board.setCnt(rs.getInt("CNT"));
-				boardList.add(board);
-			} 
-		}catch(Exception e) {
-			e.printStackTrace();
-		} finally {
-			JDBCUtil.close(rs, stmt, conn);
-		}
-		return boardList;
+		return mybatis.selectList("Board.getBoardList", vo);
 	}
 }
